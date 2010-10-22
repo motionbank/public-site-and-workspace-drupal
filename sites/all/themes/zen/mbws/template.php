cc4fba9985539e7d9090f28bd60e613fad3d9ce2
Florian Jenett <mail@florianjenett.de>
2010-10-22 11:59:26 +0200

    highlight menu items based on path added to template.php

--- sites/all/themes/zen/mbws/template.php
+++ sites/all/themes/zen/mbws/template.php
@@ -1,67 +1,53 @@
 <?php
-// $Id: template.php,v 1.21 2009/08/12 04:25:15 johnalbin Exp $
+/* See STARTERKIT/template.php for comments and help */
 
 /**
- * @file
- * Contains theme override functions and preprocess functions for the theme.
- *
- * ABOUT THE TEMPLATE.PHP FILE
- *
- *   The template.php file is one of the most useful files when creating or
- *   modifying Drupal themes. You can add new regions for block content, modify
- *   or override Drupal's theme functions, intercept or make additional
- *   variables available to your theme, and create custom PHP logic. For more
- *   information, please visit the Theme Developer's Guide on Drupal.org:
- *   http://drupal.org/theme-guide
- *
- * OVERRIDING THEME FUNCTIONS
- *
- *   The Drupal theme system uses special theme functions to generate HTML
- *   output automatically. Often we wish to customize this HTML output. To do
- *   this, we have to override the theme function. You have to first find the
- *   theme function that generates the output, and then "catch" it and modify it
- *   here. The easiest way to do it is to copy the original function in its
- *   entirety and paste it here, changing the prefix from theme_ to mbws_.
- *   For example:
- *
- *     original: theme_breadcrumb()
- *     theme override: mbws_breadcrumb()
- *
- *   where mbws is the name of your sub-theme. For example, the
- *   zen_classic theme would define a zen_classic_breadcrumb() function.
- *
- *   If you would like to override any of the theme functions used in Zen core,
- *   you should first look at how Zen core implements those functions:
- *     theme_breadcrumbs()      in zen/template.php
- *     theme_menu_item_link()   in zen/template.php
- *     theme_menu_local_tasks() in zen/template.php
- *
- *   For more information, please visit the Theme Developer's Guide on
- *   Drupal.org: http://drupal.org/node/173880
- *
- * CREATE OR MODIFY VARIABLES FOR YOUR THEME
- *
- *   Each tpl.php template file has several variables which hold various pieces
- *   of content. You can modify those variables (or add new ones) before they
- *   are used in the template files by using preprocess functions.
- *
- *   This makes THEME_preprocess_HOOK() functions the most powerful functions
- *   available to themers.
- *
- *   It works by having one preprocess function for each template file or its
- *   derivatives (called template suggestions). For example:
- *     THEME_preprocess_page    alters the variables for page.tpl.php
- *     THEME_preprocess_node    alters the variables for node.tpl.php or
- *                              for node-forum.tpl.php
- *     THEME_preprocess_comment alters the variables for comment.tpl.php
- *     THEME_preprocess_block   alters the variables for block.tpl.php
- *
- *   For more information on preprocess functions and template suggestions,
- *   please visit the Theme Developer's Guide on Drupal.org:
- *   http://drupal.org/node/223440
- *   and http://drupal.org/node/190815#template-suggestions
+ *  Highlight main menu items based on current path
+ *  fjenett - 2010-10-22
+ *  
+ *  http://api.drupal.org/api/function/theme_menu_item
  */
-
+function mbws_menu_item ( $link, $has_children, $menu = '',
+                          $in_active_trail = FALSE,
+                          $extra_class = NULL )
+{
+  global $language;
+  
+  $class = ($menu ? 'expanded' : ($has_children ? 'collapsed' : 'leaf'));
+  
+  if (!empty($extra_class))
+  {
+    $class .= ' '. $extra_class;
+  }
+  
+  $path_link = preg_replace( '/.*href="([^"]+)".*/', '\1', $link);
+  
+  $b = base_path();
+  $path_link = substr( $path_link, strlen($b) );
+  
+  $l = $language->language . '/';
+  
+  if ( strpos($path_link, $l) === 0 )
+  {
+    $path_link = str_replace( $l, '', $path_link );
+  }
+  
+  // singularize
+  // projects => project
+  // users/all => user/all
+  $path_link_single = preg_replace( '/^([^\/]+)s(\/.*)*$/i', '\1\2', $path_link);
+  
+  $path_now  = drupal_get_path_alias( urldecode( $_GET['q'] ), $language->language );
+  
+  if ( $in_active_trail || !empty($path_now) && 
+      ( (!empty($path_link) && strpos( $path_now, $path_link  ) === 0 ) ||
+        (!empty($path_link_single) && strpos( $path_now, $path_link_single  ) === 0 ) ) )
+  {
+    $class .= ' active-trail';
+  }
+  
+  return '<li class="'. $class .'">'. $link . $menu ."</li>\n";
+}
 
 /**
  * Implementation of HOOK_theme().
