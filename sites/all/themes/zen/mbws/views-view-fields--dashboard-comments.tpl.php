e50a5febcad6248212e57f2df6f6222dd22ae5cd
Florian Jenett <mail@florianjenett.de>
2010-10-24 10:06:30 +0200

    dashboard first steps. Closes #13

---
+++ sites/all/themes/zen/mbws/views-view-fields--dashboard-comments.tpl.php
@@ -0,0 +1,21 @@
+<?php
+  /* See views-view-fields.tpl.php */
+?>
+<?php
+  $field = $fields['subject'];
+  $node_title = $fields['title'];
+  $type = strtolower( str_replace( ' ', '-', $fields['type']->content ) );
+?>
+  <div class="views-field-title content-type-<?php print $type; ?>">
+      <span class="field-content">
+      <?php if ( $type !== 'wiki-page' ) : ?>
+        <?php print $field->content; ?>
+      <?php else: ?>
+        <?php print l( $field->raw, 'node/'.$fields['nid']->raw.'/talk', array( 'fragment' => 'comment-'.$fields['cid']->raw ) ); ?>
+      <?php endif; ?>
+        <span class="node-info">
+          in <span class="content-type-label"><?php print $fields['type']->content; ?></span>
+          »<span class="node-title"><?php print $node_title->content; ?></span>«
+        </span>
+      </span>
+  </div>
