65ff5b5962d94942333c2176c98697d5049133d7
Philip Reul <philip_reul@web.de>
2012-11-21 15:12:43 +0100

    search-bar code finished

--- sites/all/themes/mbworkspace/js/switch-search-form.js
+++ sites/all/themes/mbworkspace/js/switch-search-form.js
@@ -2,8 +2,14 @@ jQuery(function(){
 	/* show/hide search field block when search link is clicked. see template.php _menu_link() */
 	jQuery('.search-menu-link').click(function(event){
 		event.preventDefault();
-		jQuery('#block-search-form').toggle();
+
 		jQuery('#header').toggleClass('search-form-visible');
+		if( jQuery('#header').hasClass('search-form-visible') ){
+			jQuery('#header').animate({height: "162px"},500);
+		}
+		else{
+			jQuery('#header').animate({height: "133px"},500);
+		}	
 	});
 });
 	
