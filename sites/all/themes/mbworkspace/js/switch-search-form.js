cbad0acab7293bb21e2828fb96b1aad7fb36beb8
Philip Reul <philip_reul@web.de>
2012-11-21 14:04:12 +0100

    search-bar

--- sites/all/themes/mbworkspace/js/switch-search-form.js
+++ sites/all/themes/mbworkspace/js/switch-search-form.js
@@ -2,6 +2,7 @@ jQuery(function(){
 	jQuery('.search-menu-link').click(function(bla){
 		bla.preventDefault();
 		jQuery('#block-search-form').toggle();
+		jQuery('#header').toggleClass('search-form-visible');
 	});
 });
 	
