ede506430edbbc5d107b63c97be10c1e7229a01a
Philip Reul <philip_reul@web.de>
2013-01-18 12:42:03 +0100

    mborg moved +supersized

--- sites/all/themes/mborg/theme-settings.php
+++ sites/all/themes/mborg/theme-settings.php
@@ -7,15 +7,15 @@
  * @param $form_state
  *   A keyed array containing the current state of the form.
  */
-function zenbase_form_system_theme_settings_alter(&$form, &$form_state)  {
+function mborg_form_system_theme_settings_alter(&$form, &$form_state)  {
 
   // Create the form using Forms API: http://api.drupal.org/api/7
 
   /* -- Delete this line if you want to use this setting
-  $form['STARTERKIT_example'] = array(
+  $form['mborg_example'] = array(
     '#type'          => 'checkbox',
-    '#title'         => t('STARTERKIT sample setting'),
-    '#default_value' => theme_get_setting('STARTERKIT_example'),
+    '#title'         => t('mborg sample setting'),
+    '#default_value' => theme_get_setting('mborg_example'),
     '#description'   => t("This option doesn't do anything; it's just an example."),
   );
   // */
