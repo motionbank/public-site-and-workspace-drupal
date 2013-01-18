5f582c52c36396b045791f5d473fbe18f3d16858
Philip Reul <philip_reul@web.de>
2013-01-18 16:53:02 +0100

    template for backgroundimages

--- sites/all/themes/mborg/templates/views-view-fields--backgroundimages--block.tpl.php
+++ sites/all/themes/mborg/templates/views-view-fields--backgroundimages--block.tpl.php
@@ -24,13 +24,6 @@
  * @ingroup views_templates
  */
 ?>
-<?php foreach ($fields as $id => $field): ?>
-  <?php if (!empty($field->separator)): ?>
-    <?php print $field->separator; ?>
-  <?php endif; ?>
-
-  <?php print $field->wrapper_prefix; ?>
-    <?php print $field->label_html; ?>
-    <?php print $field->content; ?>
-  <?php print $field->wrapper_suffix; ?>
+<?php foreach ($fields as $id => $field): ?>  
+    <?php print $field->content; ?>,
 <?php endforeach; ?>
