230e4a5ebe28470bf5d478b786888cb134a6737f
Florian Jenett <fjenett@gmail.com>
2013-01-20 10:20:46 +0100

    untracked changes

--- sites/all/themes/mborg/js/bgimageslideshow.js
+++ sites/all/themes/mborg/js/bgimageslideshow.js
@@ -1,9 +1,24 @@
 /*
-
-	
+	bg image slideshow.
 */
 jQuery(function(){
-	//alert(moba_background_imgs.length);
-	//jQuery('#body').css('background-image','url(http://127.0.0.1/motionbank/sites/motionbank.org/files/error.png)');
-	jQuery('#page').css('background','url(http://127.0.0.1/motionbank/sites/motionbank.org/files/error.png)');
+	var bgImgsPreloaded = [];
+	var preloadImage = function ( imgSrc, i, cb ) {
+		var img = new Image();
+		img.src = imgSrc;
+		img.onload = function () {
+			cb(img, i);
+		}
+	}
+	if ( moba_background_imgs ) {
+		for ( var i = 0; i < moba_background_imgs.length; i++ ) {
+			if ( !bgImgsPreloaded[i] ) {
+				preloadImage(moba_background_imgs[i], i, function(img, n){
+					bgImgsPreloaded[n] = img;
+					console.log( jQuery('#bgimages') );
+					jQuery('#page').css({'background-image':'url(\''+img.src+'\')'});
+				});
+			}
+		}
+	}
 });
