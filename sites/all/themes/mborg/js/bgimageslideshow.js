3e1b348066c89c656defa4a5f46b34b98855cdec
Florian Jenett <fjenett@gmail.com>
2013-01-22 17:29:11 +0100

    bg js fixes

--- sites/all/themes/mborg/js/bgimageslideshow.js
+++ sites/all/themes/mborg/js/bgimageslideshow.js
@@ -1,47 +1,19 @@
-/*
-
-	
-*/
-
 jQuery(function(){
 
-var max_bgimages = moba_background_imgs.length;
-  
-  window.onload = function start() {
-    changebackground();
-  }
-  
-  function changebackground() {
-
-      var current_bgimage = 0;
-      window.setInterval(function () {
-        jQuery('#bgimages').css('background-image', 'url(' + moba_background_imgs[current_bgimage] + ')');
-
-        if(current_bgimage < max_bgimages){
-          current_bgimage = current_bgimage+1;
-        }
-        else {
-          current_bgimage = 0;
-        }
-        
-      }, 5000); 
-
-  }
-
-/*
- // alert(moba_background_imgs.length);
-	//jQuery('#body').css('background-image','url(http://127.0.0.1/motionbank/sites/motionbank.org/files/error.png)');
-	//jQuery('#page').css('background','url(http://127.0.0.1/motionbank/sites/motionbank.org/files/error.png)');
-
-
+  var max_bgimages = moba_background_imgs.length;
   var current_bgimage = 0;
+  var bgImages = jQuery('#bgimages');
 
-  var test = setInterval(function() {
-        jQuery('#bgimages').css('background-image', 'url(' + moba_background_imgs[current_bgimage] + ')');
-        if(current_bgimage < max_bgimages){
-          current_bgimage = current_bgimage+1;
-        }
-    }, 2000);
-    
-    */
+  function changebackground() {
+    bgImages.css('background-image', 'url(' + moba_background_imgs[current_bgimage] + ')');
+
+    if ( current_bgimage < max_bgimages-1 ) {
+      current_bgimage = current_bgimage+1;
+    } else {
+      current_bgimage = 0;
+    }
+  }
+
+  setInterval( changebackground, 5000 );
+  changebackground();
 });
