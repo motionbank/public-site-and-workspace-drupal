b99894a002c7972a93b644ca90f5f06836865378
skyliner <philip_reul@web.de>
2013-03-25 13:54:08 -0500

    fixing menu with custom js (pending) + menu styling

---
+++ sites/all/themes/mborg/js/nice-menu-custom-hover.js
@@ -0,0 +1,32 @@
+/*
+
+ */
+
+jQuery(function(){
+/*
+	
+	jQuery('.menuparent').filter(this '> ul:hidden').find('a').mouseenter(function() {
+		alert("HELLO");
+	});
+
+	*/
+
+	/*
+	jQuery('.menuparent > ul li').mouseenter(function() {
+		alert('hello');
+	//	jQuery('#nice-menu-1 .active-trail > ul').fadeTo('fast',0);
+		/*if( jQuery('.active-trail').has('ul').length != 0 ){
+			jQuery('ul', '.active-trail').children().fadeTo('fast',0.2);
+		}
+	});*/
+/*
+	jQuery('.menuparent').not('.active-trail').mouseout(function() {
+		jQuery('#nice-menu-1 .active-trail > ul').fadeTo('fast',1);
+	});
+/*
+	/* check if a submenu is open 
+	if( jQuery('.active-trail').has('ul').length != 0 ){
+		
+	}
+	*/
+});
