ab1e8bb33eeff3517d8a1f4e4ace8b61d91bbd5a
skyliner <philip_reul@web.de>
2013-05-07 12:41:29 +0200

    added hover effect for boxes

--- sites/all/themes/mborg/js/block-menu.js
+++ sites/all/themes/mborg/js/block-menu.js
@@ -10,7 +10,6 @@
  		   '#block-views-random-nodes-block').each(function(num,item){
  		item = jQuery(item);
  		var rows = jQuery('.views-row',item);
- 		console.log( rows );
  		rows.each(function(rowNum, row){
  			row = jQuery(row);
  			if ( row.data('block-preview-image-src') ) {
@@ -18,13 +17,15 @@
 		 		var img_css_url = 'url(\'' + row.data('block-preview-image-src') + '\')';
 		 		// hover states
 		 		row.hover(function(){
-		 			item_anchor.css({
-		 				'background-image':img_css_url
-		 			});
+		 			// item_anchor.css({
+		 			// 	'background-image':img_css_url
+		 			// });
+		 			jQuery('.block-bg-image', this).fadeIn(400);
 		 		},function(){
-		 			item_anchor.css({
-		 				'background-image':'none'
-		 			});
+		 			// item_anchor.css({
+		 			// 	'background-image':'none'
+		 			// });
+		 			jQuery('.block-bg-image', this).fadeOut(400);
 		 		});
 		 		jQuery( 'a', row ).css({
 		 			height:(row.data('block-preview-image-height')/2)+'px',
