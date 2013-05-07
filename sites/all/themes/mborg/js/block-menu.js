6dbbbdb99b17dc443af84eac4e5584208baa5565
skyliner <philip_reul@web.de>
2013-05-07 14:56:15 +0200

    style fix of upcomig and documentation page

--- sites/all/themes/mborg/js/block-menu.js
+++ sites/all/themes/mborg/js/block-menu.js
@@ -14,18 +14,30 @@
  			row = jQuery(row);
  			if ( row.data('block-preview-image-src') ) {
 	 			var item_anchor = jQuery('a',row);
-		 		var img_css_url = 'url(\'' + row.data('block-preview-image-src') + '\')';
+		 		//var img_css_url = 'url(\'' + row.data('block-preview-image-src') + '\')';
 		 		// hover states
 		 		row.hover(function(){
 		 			// item_anchor.css({
 		 			// 	'background-image':img_css_url
 		 			// });
 		 			jQuery('.block-bg-image', this).fadeIn(400);
+		 			jQuery({alpha:1}).animate({alpha:0}, {
+						        duration: 200,
+						        step: function(){
+						            item_anchor.css('border-color','rgba(255,255,255,'+this.alpha+')');
+						        }
+						    });
 		 		},function(){
 		 			// item_anchor.css({
 		 			// 	'background-image':'none'
 		 			// });
 		 			jQuery('.block-bg-image', this).fadeOut(400);
+		 			jQuery({alpha:0}).animate({alpha:1}, {
+						        duration: 200,
+						        step: function(){
+						            item_anchor.css('border-color','rgba(255,255,255,'+this.alpha+')');
+						        }
+						    });
 		 		});
 		 		jQuery( 'a', row ).css({
 		 			height:(row.data('block-preview-image-height')/2)+'px',
