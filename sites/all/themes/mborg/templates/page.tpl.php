30eb6733beca9c0327120fe6afd3de32f67266f6
skyliner <philip_reul@web.de>
2013-05-29 18:42:53 +0200

    link correction

--- sites/all/themes/mborg/templates/page.tpl.php
+++ sites/all/themes/mborg/templates/page.tpl.php
@@ -171,9 +171,9 @@
   <div id="footer">
     <div id="footer-lvl0">
       <ul id="footer-links">
-        <li><a href="content/imprint">IMPRINT</a></li>
-        <li><a href="content/team">TEAM</a></li>
-        <li><a href="content/contact">CONTACT</a></li>
+        <li><a href="en/content/imprint">IMPRINT</a></li>
+        <li><a href="en/content/team">TEAM</a></li>
+        <li><a href="en/content/contact">CONTACT</a></li>
       </ul>
 
       <div id="footer-spacer"></div>
